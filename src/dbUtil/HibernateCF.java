c5d25d33cac268ee3884645186bf6fef2befe3ef
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-02-11 23:16:54 +0800

    updated HibernateCF to make it reusable with other classes

--- src/dbUtil/HibernateCF.java
+++ src/dbUtil/HibernateCF.java
@@ -3,17 +3,16 @@ package dbUtil;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.models.modelExample;
 
 public class HibernateCF {
 	static SessionFactory sessionFactory = null;
 	
-	public static SessionFactory getSessionFactory() {
+	public static SessionFactory getSessionFactory(Class modelClass) {
 		
 		if (sessionFactory == null) {
 			Configuration config = new Configuration();
 			config.configure("hibernate.cfg.xml");
-			config.addAnnotatedClass(modelExample.class);
+			config.addAnnotatedClass(modelClass);
 			
 			sessionFactory = config.buildSessionFactory();
 		}
