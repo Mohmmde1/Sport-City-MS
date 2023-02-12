3ca10c2ace6f9d9e83e9593cd7e39c611f6504cb
Ayoub Ennabili <93245603+AyoubEnnabili@users.noreply.github.com>
2023-02-12 14:39:39 +0800

    remove unused imports

--- src/com/controllers/EquipmentsController.java
+++ src/com/controllers/EquipmentsController.java
@@ -2,7 +2,6 @@ package com.controllers;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +10,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.models.Equipment;
 import com.models.Facility;
