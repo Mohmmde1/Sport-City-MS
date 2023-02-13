56149c7b426d6b2267d48f51e260c5beb1f9cfc7
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-02-14 01:46:59 +0800

    reteive the user to the manage_profile & manage profile control

--- src/com/controllers/ProfileController.java
+++ src/com/controllers/ProfileController.java
@@ -1,17 +1,19 @@
 package com.controllers;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.models.Admin;
 import com.models.Feedback;
+import com.models.User;
 
 import dbUtil.HibernateCF;
 
@@ -21,29 +23,40 @@ import dbUtil.HibernateCF;
 public class ProfileController {
 	SessionFactory sessionFactory = HibernateCF.getSessionFactory();
 
-	@RequestMapping("/")
-	public String submit(HttpServletRequest request) {
-		return "manage_profile";
-	}
-
-	
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	@ResponseBody
 	public String update(HttpServletRequest request, HttpServletResponse response) {
-
+		int id = Integer.parseInt(request.getParameter("id"));
 		try (Session session = sessionFactory.openSession();) {
 
-			Feedback feedback = new Feedback();
-			feedback.setName(request.getParameter("name"));
-			feedback.setEmail(request.getParameter("email"));
-			feedback.setMessage(request.getParameter("message"));
-			System.out.println(feedback);
 			session.beginTransaction();
-			session.save(feedback);
+			User user = session.get(User.class, id);
+			user.setFname(request.getParameter("fname"));
+			user.setLname(request.getParameter("lname"));
+			user.setUsername(request.getParameter("email"));
+			user.setPhoneNumber(request.getParameter("phoneNo"));
+			if (request.getParameter("dob") != "")
+				user.setDob(request.getParameter("dob"));
+			session.update(user);
 			session.getTransaction().commit();
-			return "successfully feedback has been recorded " + feedback;
+			request.setAttribute("user", user);
+			return "manage_profile";
+
 		} catch (Exception ex) {
 			return "errorPage";
+		}
+	}
+
+	@RequestMapping("/getById")
+	public String getById(HttpServletRequest request, Model model) {
+		int id = Integer.parseInt(request.getParameter("id"));
+		try (Session session = sessionFactory.openSession()) {
+			User user = session.get(User.class, id);
+			request.setAttribute("user", user);
+			return "manage_profile";
+		} catch (Exception e) {
+			// log the error or handle it in some other way
+			e.printStackTrace();
+			return "An error occurred while trying to retrieve the admin list.";
 		}
 	}
 
