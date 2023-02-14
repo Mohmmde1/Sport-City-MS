89938c830322c1f04bd92ea30d57c4dbca8a7b05
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-02-15 05:55:23 +0800

    insert facilities updated

---
+++ migrations/08_insert_facilities.sql
@@ -0,0 +1,12 @@
+INSERT INTO facilities (name, location, capacity, status, employee_id, price) 
+VALUES 
+  ('Swimming Pool', 'Building A, Level 3', 50, 'Available', NULL, 25.99),
+  ('Basketball Court', 'Building B, Level 2', 100, 'Available', NULL, 15.5),
+  ('Tennis Court', 'Building C, Level 1', 20, 'Available', NULL, 30.0),
+  ('Gym', 'Building A, Level 1', 200, 'Available', NULL, 50.25),
+  ('Squash Court', 'Building B, Level 1', 10, 'Available', NULL, 20.0),
+  ('Yoga Studio', 'Building D, Level 2', 30, 'Available', NULL, 12.75),
+  ('Table Tennis Room', 'Building E, Level 1', 15, 'Available', NULL, 10.0),
+  ('Indoor Running Track', 'Building F, Level 3', 50, 'Available', NULL, 8.0),
+  ('Badminton Court', 'Building G, Level 2', 25, 'Available', NULL, 17.0),
+  ('Rock Climbing Wall', 'Building H, Level 1', 5, 'Available', NULL, 30.5);
